089e863cb8026415225c0ffa15871ee04155c0b3
John Hutchinson <john@upnorth.net>
2023-01-05 19:36:37 -0500

    Added config file.  added mob kills per point config

---
+++ galaxyrpmod/src/main/java/net/mcreator/galaxyrpmod/config/ModCommonConfigs.java
@@ -0,0 +1,20 @@
+package net.mcreator.galaxyrpmod.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ModCommonConfigs {
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
+
+    public static final ForgeConfigSpec.ConfigValue<Integer> MOB_KILLS_PER_POINT;
+
+    static{
+        BUILDER.push("Configs for ShinobiCraft Mod");
+
+        MOB_KILLS_PER_POINT = BUILDER.comment("How many mobs must you kill to get a Point?")
+                .define("mobs per point", 7);
+
+        BUILDER.pop();
+        SPEC = BUILDER.build();
+    }
+}
